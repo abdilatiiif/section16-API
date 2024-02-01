847c25c71ecd95a35b17ce4fd83ac7695a1e3811
Abdilatif M.A <abdilatif@MacBook-Pro.local>
2024-02-01 04:02:39 +0100

    testing my animal side

--- script.js
+++ script.js
@@ -301,5 +301,47 @@ btn.addEventListener('click', whereIm);
 */
 
 function createImage(url) {
-  return new Promise((resolve, reject) => { });
+  return new Promise((resolve, reject) => {});
+}
+
+const addAnimal = document.querySelector(".btn-animal");
+const animalCards = document.createElement("div");
+
+document.querySelector('body').append(animalCards);
+
+const animals = [
+  { name: "dog", img: "https://media.istockphoto.com/id/539150389/photo/winter-walk-of-golden-retriever-puppy.jpg?s=612x612&w=0&k=20&c=iZ06Vb4aKGbCgHWWnQzvgYyuuluhdHByN3gKPwvQrIo=" },
+  { name: "cat", img: "https://media.istockphoto.com/id/1349426354/photo/young-cat-lying-under-christmas-tree.webp?b=1&s=170667a&w=0&k=20&c=z9Wv5Ua4ZzPa89OTJnlnWZYV6Ud77AnDkjA2yXFH1EE=" },
+  { name: "elephant", img: "https://media.istockphoto.com/id/620379788/photo/african-elephant-and-the-ngorongoro-savanna-in-tanzania.webp?b=1&s=170667a&w=0&k=20&c=DGtLhqAYTMeepRTgi6chI0JmaEYD3A10uOUSkebSdOs=" },
+  { name: "snake", img: "https://media.istockphoto.com/id/624626136/photo/grass-snake.webp?b=1&s=170667a&w=0&k=20&c=XOR9jYuLaFl1b1NKHQr4h4LyRE6FFfbMYonXEJ_aPF8=" },
+  { name: "lion", img: "https://media.istockphoto.com/id/171136538/photo/male-lion-lying-on-concrete-floor-at-sunset-with-orange-wall.webp?b=1&s=170667a&w=0&k=20&c=DLloJoTKcR3seR2_3srVYX_e9v6nFqTzRc7jlYWcoRM=" },
+  { name: "zebra", img: "https://media.istockphoto.com/id/1418803312/photo/mother-zebra-and-her-cub.webp?b=1&s=170667a&w=0&k=20&c=T3wH1wE-EW_CXIjKCCAt0o1I2cROcPYblojRBBANHGo=" },
+  { name: "eagle", img: "https://media.istockphoto.com/id/168511255/photo/bald-eagle-gliding-against-blue-sky-and-white-wispy-clouds.webp?b=1&s=170667a&w=0&k=20&c=YKKSZ3lSu3h9FRcY9aN6Ee-E98i4MUmzBJZl2a0nYjU=" },
+];
+
+addAnimal.addEventListener("click", animalCard);
+
+function animalCard() {
+  let randomAnimal = Math.floor(Math.random() * animals.length);
+
+  console.log(randomAnimal);
+
+  createAnimalCard(animals[randomAnimal]);
+}
+
+function createAnimalCard(animal) {
+
+  const animalCard = document.createElement("div");
+  const animalName = document.createElement('h1');
+  animalName.classList.add('country__name');
+  animalName.innerText = `this is ${animal.name}`;
+
+  const img = document.createElement('img');
+  img.classList.add('country__img');
+
+  img.src = `${animal.img}`;
+
+  animalCard.append(animalName, img);
+
+  animalCards.append(animalCard);
 }
